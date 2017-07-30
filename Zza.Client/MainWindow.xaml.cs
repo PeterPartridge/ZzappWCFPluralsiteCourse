3913933e4ecc048e234f5763a5df98e59206fe24
Peter Partridge <petertoby@aol.com>
2017-07-30 13:39:39 +0100

    THis is notes and will not work when run

---
+++ Zza.Client/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Zza.Client.ZzaService;
+
+
+
+namespace Zza.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Products();
+
+
+
+        }
+
+        private async void Products()
+        {
+            ZzaServiceClient proxy = new ZzaServiceClient("NetTcpBinding_IZzaService");
+            // setting client credentials in the proxy.
+            proxy.ClientCredentials.Windows.ClientCredential.UserName = "Bob";
+
+            try
+            {
+               
+
+                var Products = await proxy.GetProductsAsync();
+                var Customer = await proxy.GetCustomersAsync();
+           
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+            }
+
+            finally 
+            {
+                proxy.Close();
+            }
+        }
+    }
+}
---
+++ Zza.SelfHost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Services;
+
+namespace Zza.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(ZzaService));
+
+                host.Open();
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+
+            }
+      
+        }
+    }
+}
